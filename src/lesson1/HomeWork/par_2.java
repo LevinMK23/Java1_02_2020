7299519bd2b40331b7fc245430f424ec686b31ff
Nikolay Dementyev <nikolay.v.dementyev@megafon.ru>
2020-02-24 14:51:13 +0300

    first home work

---
+++ src/lesson1/HomeWork/par_2.java
@@ -0,0 +1,16 @@
+package lesson1.HomeWork;
+
+public class par_2 {
+    static byte b = 0;
+    static short s = 20;
+    static int i = 30;
+    static long l = 40;
+    static char c = 'Y';
+    static float f = 1.23f;
+    static double d = 4.23;
+    static boolean bool = true;
+
+    public static void main(String[] args) {
+        System.out.println(i + "\n" + bool + "\n" + s + "\n" + l + "\n" + c + "\n" + f + "\n" + d);
+    }
+}
